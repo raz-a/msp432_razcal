a8cba3d53761c6e238f0c6fab7f2adf871b17695
Raz <raz-@outlook.com>
2019-12-20 02:27:06 -0800

    warning

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs;
 
 fn main() {
+    println!("cargo:warning=build");
     let config_location = env!("RAZCAL_CONFIG");
     let config_string = fs::read_to_string(config_location);
     match config_string {
@@ -11,7 +12,7 @@ fn main() {
         },
 
         Err(_) => {
-            println!("NOOOOOO");
+            println!("cargo:warning=Cannot find config");
         }
     }
 }
