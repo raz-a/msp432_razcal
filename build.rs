0a59f8faa6bdcced63537d868adee858e042ede1
Raz <raz-@outlook.com>
2020-01-21 22:18:22 -0800

    Small change

--- build.rs
+++ build.rs
@@ -4,11 +4,11 @@ use std::fs;
 
 fn main() {
     println!("cargo:warning=build");
-    let config_location = env!("RAZCAL_CONFIG");
+    let config_location = env!("RAZCAL_CONFIG", "RAZCAL_CONFIG environment variable not found");
     let config_string = fs::read_to_string(config_location);
     match config_string {
         Ok(buffer) => {
-            println!("cargo:rustc-cfg=msp432_package=\"{}\"", buffer);   
+            println!("cargo:rustc-cfg=msp432_package=\"{}\"", buffer);
         },
 
         Err(_) => {
