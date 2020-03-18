1f93b92cda85dc01adf4515aab881356ce10bb3c
Raz <raz-@outlook.com>
2020-03-18 00:00:24 -0700

    cleanup

--- src/pin.rs
+++ src/pin.rs
@@ -315,15 +315,15 @@ pub type PinSet16 = PinSet<16>;
 pub type PinSet32 = PinSet<32>;
 
 impl<const COUNT: usize> PinSet<COUNT> {
+    // TODO: Figure out how to make this as efficient as possible.
     pub fn new(pins: [PinName; COUNT]) -> Option<Self> {
         let mut pins_allocated = 0;
         let mut pin_set_uninit: [MaybeUninit<Pin>; COUNT] = MaybeUninit::uninit_array();
-
-        for (pin, name_of_pin) in pin_set_uninit.iter_mut().zip(pins.iter()) {
-            let allocated_pin = Pin::new(*name_of_pin);
+        for i in 0..COUNT {
+            let allocated_pin = Pin::new(pins[i]);
             match allocated_pin {
                 Some(p) => {
-                    *pin = MaybeUninit::new(p);
+                    pin_set_uninit[i] = MaybeUninit::new(p);
                     pins_allocated += 1;
                 },
                 None => {
@@ -346,32 +346,38 @@ impl<const COUNT: usize> PinSet<COUNT> {
     }
 
     pub fn get_pins(&self) -> [PinName; COUNT] {
-        let mut pin_names: [MaybeUninit<PinName>; COUNT] = MaybeUninit::uninit_array();
-        let map = self.pins.into_iter().map(|pin| pin.get_pin());
-        for (name, ret_name) in map.zip(pin_names.iter_mut()) {
-            *ret_name = MaybeUninit::new(name);
+        let mut pins: [MaybeUninit<PinName>; COUNT] = MaybeUninit::uninit_array();
+        let map = self.pins.iter().map(|pin| pin.get_pin());
+        let mut i = 0;
+        for pin in map {
+            pins[i] = MaybeUninit::new(pin);
+            i += 1;
         }
 
-        assume_init_const_generic_array(pin_names)
+        assume_init_const_generic_array(pins)
     }
 
     pub fn get_ports(&self) -> [u8; COUNT] {
-        let mut port_names: [MaybeUninit<u8>; COUNT] = MaybeUninit::uninit_array();
-        let map = self.pins.into_iter().map(|pin| pin.get_port());
-        for (name, ret_name) in map.zip(port_names.iter_mut()) {
-            *ret_name = MaybeUninit::new(name);
+        let mut ports: [MaybeUninit<u8>; COUNT] = MaybeUninit::uninit_array();
+        let map = self.pins.iter().map(|pin| pin.get_port());
+        let mut i = 0;
+        for port in map {
+            ports[i] = MaybeUninit::new(port);
+            i += 1;
         }
 
-        assume_init_const_generic_array(port_names)
+        assume_init_const_generic_array(ports)
     }
 
     pub fn get_pin_offsets_in_port(&self) -> [u8; COUNT] {
-        let mut offset_names: [MaybeUninit<u8>; COUNT] = MaybeUninit::uninit_array();
-        let map = self.pins.into_iter().map(|pin| pin.get_pin_offset_in_port());
-        for (name, ret_name) in map.zip(offset_names.iter_mut()) {
-            *ret_name = MaybeUninit::new(name);
+        let mut offsets: [MaybeUninit<u8>; COUNT] = MaybeUninit::uninit_array();
+        let map = self.pins.iter().map(|pin| pin.get_pin_offset_in_port());
+        let mut i = 0;
+        for offset in map {
+            offsets[i] = MaybeUninit::new(offset);
+            i += 1;
         }
 
-        assume_init_const_generic_array(offset_names)
+        assume_init_const_generic_array(offsets)
     }
 }
